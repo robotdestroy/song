77f6d6692c062b54ec76ed6c17957fe33ba27952
Charlie Deets <charlie.deets@gmail.com>
2013-04-29 11:18:35 -0500

    rem

--- song/header.php
+++ song/header.php
@@ -15,10 +15,6 @@
 <meta name="viewport" content="width=device-width" />
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
 
 <?php wp_head(); ?>
 </head>
